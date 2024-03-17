1dab23302830a13fb71e2a214c98f6f08fe61753
timwaizenegger <mail+gh@timwaizenegger.de>
2024-03-17 17:12:05 +0100

    upload and thumbnails

---
+++ jsondir.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func jsonDirList(w http.ResponseWriter, r *http.Request) {
+	dir := imagePath
+	fileInfos, err := listFiles(dir)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	jsonBytes, err := json.Marshal(fileInfos)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(jsonBytes)
+}
+
+type fileInfo struct {
+	Name      string `json:"name"`
+	ThumbName string `json:"thumb_name"`
+	ThumbPath string `json:"thumb_path"`
+}
+
+func listFiles(dir string) ([]fileInfo, error) {
+	var files []fileInfo
+	dirEntries, err := os.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+	for _, de := range dirEntries {
+		if !de.IsDir() {
+			files = append(files, fileInfo{
+				Name:      de.Name(),
+				ThumbName: fmt.Sprintf("%s%s", de.Name(), thumbSuffix),
+				ThumbPath: fmt.Sprintf("../%s%s%s", thumbPath, de.Name(), thumbSuffix),
+			})
+		}
+	}
+	return files, nil
+}
---
+++ website-photo-upload.go
@@ -0,0 +1,131 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"gopkg.in/gographics/imagick.v3/imagick"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+)
+
+// Notes:
+// export CGO_CFLAGS_ALLOW='-Xpreprocessor'
+
+var (
+	mw *imagick.MagickWand
+)
+
+const (
+	imagePath   = "./images/"
+	thumbPath   = "./images/thumbs/"
+	thumbSuffix = ".thumb.jpg"
+)
+
+func putUpload(w http.ResponseWriter, r *http.Request) {
+	log.Printf("upload %q %q", r.URL, r.Method)
+	err := r.ParseMultipartForm(1000 * 1024 * 1024)
+	if err != nil {
+		log.Printf("error parsing multipart form: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Get a reference to the file
+	_, _, err = r.FormFile("imageInputName")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// TODO: return error if any happened
+	for k, v := range r.MultipartForm.File {
+		log.Printf("got a file %s", k)
+		for _, header := range v {
+			log.Printf("got a file header %v", header.Filename)
+			err = saveUploadedFile(header)
+			if err != nil {
+				log.Printf("error saving file header: %v", err)
+			}
+		}
+	}
+
+	//io.WriteString(w, "upload processed successfully\n")
+	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+// saveUploadedFile ...
+// TODO: save as sha sum key
+func saveUploadedFile(h *multipart.FileHeader) error {
+	log.Printf("writing file to disk %q size %dKB", h.Filename, h.Size/1024)
+	f, err := h.Open()
+	if err != nil {
+		log.Printf("error opening file %q: %v", h.Filename, err)
+		return err
+	}
+	defer f.Close()
+	buffer := make([]byte, h.Size)
+	f.Read(buffer)
+	sum := sha256.Sum256(buffer)
+	ext := filepath.Ext(h.Filename)
+	if ext == "" {
+		ext = ".jpg"
+	}
+	fileName := fmt.Sprintf("%x%s", sum[:], ext)
+	targetPath := filepath.Join(imagePath, fileName)
+	out, err := os.Create(targetPath)
+	if err != nil {
+		log.Printf("error creating local file %q: %v", targetPath, err)
+		return err
+	}
+	_, err = out.Write(buffer)
+	if err != nil {
+		log.Printf("error writing to local file %q: %v", targetPath, err)
+		return err
+	}
+	makeThumbnail(targetPath)
+	return nil
+}
+
+func makeThumbnail(imagePath string) error {
+	imageDir := path.Dir(imagePath)
+	imageName := path.Base(imagePath)
+	outDir := path.Join(imageDir, "thumbs")
+	outName := fmt.Sprintf("%s%s", imageName, thumbSuffix)
+	outPath := path.Join(outDir, outName)
+
+	log.Printf("making a thumbnail for %q. Will pace the file at %q", imagePath, outPath)
+	ret, err := imagick.ConvertImageCommand([]string{
+		"convert", imagePath, "-thumbnail", "256x256", outPath,
+	})
+	if err != nil {
+		log.Printf("ERROR making a thumbnail for %q, %s", imagePath, err)
+		return err
+	}
+	log.Printf("thumbnail result: %v", ret)
+	return nil
+}
+
+func main() {
+
+	imagick.Initialize()
+	defer imagick.Terminate()
+
+	mw = imagick.NewMagickWand()
+
+	mux := http.NewServeMux()
+	mux.Handle("/html/", http.StripPrefix("/html/", http.FileServer(http.Dir("./html"))))
+	mux.Handle("/images/", http.StripPrefix("/images/", http.FileServer(http.Dir("./images/"))))
+	mux.HandleFunc("/api/thumbs", jsonDirList)
+	mux.Handle("/", http.RedirectHandler("/html/upload.html", http.StatusSeeOther))
+	mux.HandleFunc("/upload", putUpload)
+
+	//err := http.ListenAndServe("127.0.0.1:3333", mux)
+	err := http.ListenAndServe("0.0.0.0:3333", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("hallo")
+}
